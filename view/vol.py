7bc94e28a87e0c6d4db356fdb76265b81e8fc9cc
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2022-05-01 15:37:42 -0600

    Fixing some type errors when decoding views in the volumes.

--- view/vol.py
+++ view/vol.py
@@ -4,12 +4,13 @@ from util.byte import nibble
 # offset here is expected as the value provided by read_view_dir
 def get_view_loops(vol_file, view_offset):
     with open(vol_file, mode='rb') as f:
-        # seek past the offset and 5 additional bytes from the header and two additional unused bytes from the view header
+        # seek past the offset and 5 additional bytes from the header and two additional unused bytes from the view
+        # header
         f.seek(view_offset + 7)
 
-        i, num_loops, desc_bytes, loop_offsets = 0, f.read(1), f.read(2), []
+        i, num_loops, desc_bytes, loop_offsets = 0, int.from_bytes(f.read(1), 'big'), f.read(2), []
         desc_ls, desc_ms = desc_bytes
-        desc_offset = (desc_ms << 8) + desc_ls
+        desc_offset = (int.from_bytes(desc_ms, 'big') << 8) + int.from_bytes(desc_ls, 'big')
 
         while i < num_loops:
             ls, ms = f.read(2)
@@ -25,7 +26,7 @@ def get_view_cels(vol_file, loop_offsets):
     with open(vol_file, mode='rb') as f:
         for loop_offset in loop_offsets:
             f.seek(loop_offset, 0)
-            i, num_cells = 0, f.read(1)
+            i, num_cells = 0, int.from_bytes(f.read(1), 'big')
 
             while i < num_cells:
                 ls, ms = f.read(2)
@@ -42,13 +43,8 @@ def get_cel_data(vol_file, offset_pairs):
         for loop_offset, cel_offset in offset_pairs:
             f.seek(cel_offset, loop_offset)
 
+            # make sure to call draw_cel_data and enforce that we only pass in the cel data as long as the number of
+            # 0x00's is < height
             width, height, alpha_mirroring = f.read(3)
             width, mirror, alpha = width * 2, nibble(alpha_mirroring, 'lo'), nibble(alpha_mirroring, 'hi')
             cels.append((width, height, mirror, alpha))
-
-
-
-
-
-
-
