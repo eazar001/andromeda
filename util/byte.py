13410f4786388b645ad828232bf413919ebf72e5
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2022-05-02 17:26:51 -0600

    Fixing util.byte.nibble

--- util/byte.py
+++ util/byte.py
@@ -1,5 +1,2 @@
-def nibble(byte, section, endian='big'):
-    if endian == 'little':
-        section = 'lo' if section == 'hi' else section
-
+def nibble(byte, section):
     return (240 & byte) >> 4 if section == 'hi' else 15 & byte
