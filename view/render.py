db8ddf0a40a7811efaa57b63806a1e36f357b85e
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2025-08-27 20:46:21 -0600

    Updating signature and comments in draw_cel_data for clarity.

--- view/render.py
+++ view/render.py
@@ -11,18 +11,20 @@ palette = [
 ]
 
 
-def draw_cel_data(renderer, x, y, pixels, alpha):
-    width = x
+def draw_cel_data(renderer, width, height, pixels, alpha):
+    x, y = width, height
     # using the width and height information from the cel header, we can stop here when y = n - 1,
     # or when number of pixels drawn = x * y
     for color, num_pixels in pixels:
         if color == 0 == num_pixels:
+            # we've finished a line, so reset x to the beginning and move y down
             x, y = width, y + 1
             continue
 
         (r, g, b), n = palette[color], x + 2 * num_pixels
         renderer.color = Color(r, g, b, 0xFF) if color != alpha else Color(r, g, b, 0x00)
         renderer.draw_point(points=[(x0, y) for x0 in range(x, n)])
+        # because we drew all the way to n, we need to set x to n here
         x = n
 
 
