85c7e82fc02bd6fc17baf47fbd902abfa2dfeff8
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2026-04-12 22:35:57 -0600

    Adding mirror utility.

--- util/byte.py
+++ util/byte.py
@@ -1,2 +1,6 @@
 def nibble(byte, section):
     return (240 & byte) >> 4 if section == 'hi' else 15 & byte
+
+# returns mirror bit, and the non-mirror loop index, given a nibble
+def cel_mirror(n):
+    return n >> 3, n & 7
